739d367a50b8398011aba6d0d338f5e08004214b
Sayali267 <39720151+Sayali267@users.noreply.github.com>
2020-07-19 22:57:56 +0530

    JS Assignment 4

---
+++ JS day 4/q6.js
@@ -0,0 +1,25 @@
+
+while(true){
+    let n = parseFloat(prompt('Enter a number greater than 100',580));
+    if(n>=100){
+        console.log(`The number is ${n}`);
+        break;
+    }
+    else{
+        console.log(`The number entered i.e ${n} is wrong`);
+        continue;
+    }
+
+    
+    
+    
+        
+
+    
+}
+    
+    
+
+
+
+
